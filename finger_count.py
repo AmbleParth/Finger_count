f96c0dad605d6195d0125574b1eaa33c6ff9e9e8
Parth Amble <94055399+AmbleParth@users.noreply.github.com>
2023-07-05 14:00:24 +0530

    Add files via upload
    This is an advance computer vision project which counts the finger displayed on screen via webcam

---
+++ finger_count.py
@@ -0,0 +1,69 @@
+import cv2
+import os
+import time
+import HandTrackingModule as htm
+wCam,hCam = 1000,1000
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+folderPath = "C://Users//SARTHAK AMBLE//Desktop//Python//Finger_images//images"
+
+myList = os.listdir(folderPath)
+print(myList)
+
+overLayList = []
+
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    #print(f'{folderPath}/{imPath}')
+    overLayList.append(image)
+
+print(len(overLayList))
+pTime = 0
+
+detector = htm.handDetector(detectionCon = 0.75)
+tipIds = [4,8,12,16,20]
+
+
+while True :
+    success,img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img,draw=False)
+    #print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+        h, w, c = overLayList[totalFingers - 1].shape
+        img[0:h, 0:w] = overLayList[totalFingers - 1]
+
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                    10, (255, 0, 0), 25)
+
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,f'FPS:{int(fps)}',(400,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+    cv2.imshow("image",img)
+    cv2.waitKey(1)
